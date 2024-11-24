1e68d328b27f41e13580ecc1ac9cd0d60bd60e0f
oyenolaphilipinc <oyenolaphilip89@gmail.com>
2024-11-24 07:34:28 -0800

    fixed

--- lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { GetServerSidePropsContext } from 'next';
-import { parseCookies } from 'next-cookies';
-
-export function isAuthenticated(ctx: GetServerSidePropsContext): boolean {
-  const cookies = parseCookies(ctx);
-  return cookies.auth === 'true';
-}
