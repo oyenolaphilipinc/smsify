9b6e38828365f3ec445ea45405ee44c2e2f9fc35
Oyenola Philip <76276196+oyenolaphilipinc@users.noreply.github.com>
2025-01-14 17:37:24 +0100

    Fix
    Fix

--- components/ProtectedLayout.tsx
+++ components/ProtectedLayout.tsx
@@ -40,7 +40,7 @@ export default function ProtectedLayout({
         <div className='md:ml-60'>
           <Navbar />
         </div>
-        <div className='md:ml-60'>
+        <div className='md:ml-64'>
           {children}
         </div>
       </main>
