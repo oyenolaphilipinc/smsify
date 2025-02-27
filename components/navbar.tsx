4b188017a58dc578d9dcf0ffd2f0383777b07354
oyenolaphilipinc <oyenolaphilip89@gmail.com>
2025-02-27 05:13:52 -0800

    Little fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -98,7 +98,7 @@ export function Navbar() {
                 </Link>
               ))}
               <Link
-                href="/signup"
+                href={user ? "/signup" : "/dashboard"}
                 onClick={() => setIsOpen(false)}
                 className="bg-primary text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-primary/90"
               >
